6eb6f0b04fd1e17daa212e5e5f47baac1baec7cc
Marina Gaisina <farigatovna@hotmail.com>
2022-01-23 21:53:31 -0500

    login page check validation done

--- main.js
+++ main.js
@@ -1,2 +1,44 @@
-// let login = document.getElementsByClassName('loginform');
-// login.addEventListener(e -> e.)
+'use strict';
+const loginFormOpenClose = () => {
+    const article = document.getElementById("article_display");
+    if (article.classList.contains('d-none')) {
+        article.classList.remove('d-none');
+        article.classList.add("d-block");
+    } else {
+        article.classList.remove('d-block');
+        article.classList.add('d-none');
+    }
+}
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+let forms = document.querySelectorAll('.needs-validation')
+
+// Loop over them and prevent submission
+Array.prototype.slice.call(forms)
+    .forEach((form) => {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            form.classList.add('was-validated');
+        }, false)
+    })
+
+
+//HTML: onclick="checkValidity(document.login_form.login.value, document.login_form.pass.value)"
+
+//  const checkValidity = (email, pass) => {
+//     let isEmailValid = validateEmail(email);
+//     let isPassValid = (pass.length < 5);
+//     if (!isEmailValid || !isPassValid) {
+//         return false;
+//     }
+//     return true;
+//  }
+//
+// const validateEmail = (email) => {
+//     let atpos = email.indexOf("@");
+//     let dotpos = email.lastIndexOf(".");
+//     return !(atpos < 1 || (dotpos - atpos < 2));
+// }
